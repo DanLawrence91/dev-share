b1d733adf5e31631deede8b09e22b33cb2be1a6a
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-05-09 11:30:10 +0930

    adding typedefs for project

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,35 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    _id: ID
+    firstname: String
+    surname: String
+    username: String
+    email: String
+    password: String
+    github: String
+    projects: [Project]!
+  }
+
+  type Project {
+    _id: ID
+    title: String
+    description: String
+    technology: String
+    link: String
+    owner: String
+    comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+  }
+`;
 
 module.exports = typeDefs;
+
+// owner in tech may be user - need to see how to link best
+// also need to add in auth
